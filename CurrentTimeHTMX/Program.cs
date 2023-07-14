74bfd49b3707f5ad43b5be9a1a48237a5b8fd6dd
salma-ahmed9 <93517375+salma-ahmed9@users.noreply.github.com>
2023-07-14 03:52:14 +0200

    code added

---
+++ CurrentTimeHTMX/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/datetime", () =>
+{
+    DateTimeOffset currentDateTimeOffset = DateTimeOffset.Now;
+    string currentDateTime = currentDateTimeOffset.ToString("yyyy-MM-dd HH:mm:ss zzz");
+    return Results.Text(currentDateTime, "text/html");
+});
+/*app.MapFallback(context =>
+{
+    context.Response.Redirect("/datetime");
+    return Task.CompletedTask;
+});*/
+app.Run();
